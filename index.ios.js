0ef9da0ae3808b67e7f9217582058b57c09244b2
hborism <henrik.borismoller@gmail.com>
2016-06-15 14:33:33 +0200

    First commit

---
+++ index.ios.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+} = FBSDK;
+
+// var Login = React.createClass({
+//   render: function() {
+//     return (
+//       <View>
+//         <LoginButton
+//           publishPermissions={["publish_actions"]}
+//           onLoginFinished={
+//             (error, result) => {
+//               if (error) {
+//                 alert("Login failed with error: " + result.error);
+//               } else if (result.isCancelled) {
+//                 alert("Login was cancelled");
+//               } else {
+//                 alert("Login was successful with permissions: " + result.grantedPermissions)
+//               }
+//             }
+//           }
+//           onLogoutFinished={() => alert("User logged out")}/>
+//       </View>
+//     );
+//   }
+// });
+
+class YourApp extends Component {
+  render() {
+    return (
+      <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
+          }
+          onLogoutFinished={() => alert("User logged out")}/>
+      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //     Welcome to React Native!
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit index.ios.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     Press Cmd+R to reload,{'\n'}
+      //     Cmd+D or shake for dev menu
+      //   </Text>
+      // </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('YourApp', () => YourApp);
